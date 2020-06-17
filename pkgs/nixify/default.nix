da7e47ab1fa555a3f8ed23697fb23f1a4f54788d
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2020-06-17 18:21:08 +0100

    nixify script

---
+++ pkgs/nixify/default.nix
@@ -0,0 +1,32 @@
+{ direnv, stdenv, runCommand, writeScriptBin }:
+
+let 
+  defaultShellNix = ''
+    { pkgs ? import <nixpkgs> {} }:
+
+    pkgs.mkShell {
+      buildInputs = [ ];
+    }
+  '';
+  script = ''
+    #!${stdenv.shell}
+
+    if [ ! -f ./shell.nix -a ! -f ./default.nix ]; then
+      cp @out@/shell.nix ./shell.nix
+    fi
+
+    echo "use_nix" > .envrc
+    ${direnv}/bin/direnv allow .
+  '';
+
+in
+
+runCommand "nixify" {
+  inherit script defaultShellNix;
+  passAsFile = [ "script" "defaultShellNix" ];
+} ''
+  mkdir -p $out/bin
+  substitute $scriptPath $out/bin/nixify --subst-var out
+  chmod +x $out/bin/nixify
+  cp $defaultShellNixPath $out/shell.nix
+''
